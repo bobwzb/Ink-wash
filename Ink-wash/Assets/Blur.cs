c53afa435766bb0a250f8698d84ab799c01b2e81
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-08-09 15:27:04 -0700

    add blur shader

---
+++ Ink-wash/Assets/Blur.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blur : PostEffectsBase
+{
+    public Shader gaussianBlurShader;
+    private Material gaussianBlurMaterial = null;
+
+    public Material material
+    {
+        get
+        {
+            gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+            return gaussianBlurMaterial;
+        }
+    }
+    [Range(0, 4)]
+    public int iterations = 3;
+
+    // Blur spread for each iteration - larger value means more blur
+    [Range(0.2f, 3.0f)]
+    public float blurSpread = 0.6f;
+
+    [Range(1, 8)]
+    public int downSample = 2;
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            int rtW = src.width / downSample;
+            int rtH = src.height / downSample;
+            RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+            buffer.filterMode = FilterMode.Bilinear;
+
+            // Render the vertical pass
+            Graphics.Blit(src, buffer, material, 0);
+            // Render the horizontal pass
+            Graphics.Blit(buffer, dest, material, 1);
+
+            RenderTexture.ReleaseTemporary(buffer);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+
+}
